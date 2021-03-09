5fbc5de2227b0e569fee126eb866a42354d2f925
Stefanie Dziadkowiec <stefaniemarie.dz@gmail.com>
2021-03-09 11:35:35 +0100

    styling the product pg

--- pages/food/[id].js
+++ pages/food/[id].js
@@ -1,13 +1,22 @@
 //as long as this files starst with brackets, it is dynamic. This is a Next thing.
 import firebaseInstance from '../../config/firebase'
 import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import PageMenu from '../../components/PageMenu'
+import { Wrapper } from '../../components/Wrapper'
 
 function FoodPage({ item, error }) {
-  const [selectedItem, setSelectedItem] = useState(item)
+  //const [selectedItem, setSelectedItem] = useState(item)
 
   return (
     <>
-      <h1>you are looking at {selectedItem.name}</h1>
+      <PageMenu />
+      <Wrapper>
+        <ItemContainer>
+          <h2>{item.name}</h2>
+          <img id={item.type === 'drink' ? 'drink' : ''} src={item.pic}></img>
+        </ItemContainer>
+      </Wrapper>
     </>
   )
 }
@@ -27,5 +36,25 @@ FoodPage.getInitialProps = async ({ query }) => {
     return { error: error.message }
   }
 }
+
+const ItemContainer = styled.article`
+  h2 {
+    font-size: 3.5rem;
+    color: #ffba6a;
+  }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 1px solid white;
+  height: 60vh;
+
+  #drink {
+    height: 200px;
+    width: auto;
+  }
+  img {
+    width: 250px;
+  }
+`
 
 export default FoodPage
